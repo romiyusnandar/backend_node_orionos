2e2b56c372a571372e0fcf35741807b91ee64ff6
romiyusnandar <yusromi04@gmail.com>
2025-01-03 07:57:20 +0700

    Configure ```cors``` and ```body-parser``` for access and response

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = 4000;
 
